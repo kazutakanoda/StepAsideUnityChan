72041ae398eabcdd7fd39755eac09254f636a5b7
野田和孝 <k.noda0407@gmail.com>
2017-07-22 23:53:32 +0900

    StepAsideUnityChanの課題提出
    Lesson6の「課題：不要になったアイテムを順次破棄しましょう」を追記したデータです。

---
+++ Assets/ItemDestroyController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDestroyController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GameObject.Find("Main Camera").transform.position.z - this.transform.position.z > 0) {
+			Destroy (gameObject);
+		}
+	}
+
+
+}
+
